f51bedc3e4f239bd93af0af310e696a956bbaf79
MickyWillys <fozeum@yahoo.fr>
2015-02-18 03:38:09 +0100

    first-part-almost-done

--- carousel.js
+++ carousel.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
   $('.mon_carousel').slick({
+  	arrows: true,
+  	autoplay: true,
+  	autoplaySpeed: 6000,
+  	fade: true,
     dots: true,
-    infinite: true,
-    speed: 500,
+    infinite: true,	
     slidesToShow: 1,
     slidesToScroll: 1
   });
